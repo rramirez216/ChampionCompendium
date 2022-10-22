e8d79aa2db283a90c127fd8ccb688dbc9f73c7e7
Ruben Ramirez <rramirez110911@gmail.com>
2022-10-21 22:08:55 -0700

    changes css from display grid to display flex

--- champion-search/src/components/ChampionList.js
+++ champion-search/src/components/ChampionList.js
@@ -3,14 +3,24 @@ import ListItem from './ListItem'
 
 const ChampionList = ({ list }) => {
   return (
-    <section>
+    <Section>
       {list ? (
         list.map((value) => <ListItem name={value.name} id={value.id} />)
       ) : (
         <div>loading...</div>
       )}
-    </section>
+    </Section>
   )
 }
+
+const Section = styled.div`
+  max-width: 1440px;
+  /* display: grid;
+  grid-template-columns: repeat(5, minmax(150px, 1fr));
+  grid-template-rows: repeat(auto, 1fr); */
+  display: flex;
+  flex-flow: row wrap;
+  gap: 16px;
+`
 
 export default ChampionList
