04b01f4e8d274c24202ad65e2ffa428f18e144d5
Ruben Ramirez <rramirez110911@gmail.com>
2022-10-31 21:50:53 -0700

    adds a handle search function and passes it as props

--- champion-search/src/App.js
+++ champion-search/src/App.js
@@ -1,18 +1,43 @@
 import { useState } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import getValuesFromResponse from './utils/getValuesFromResponse'
+import useFetch from './hooks/useFetch'
 import Home from './pages/Home'
 import Champion from './pages/Champion'
 import GlobalStyles from './components/GlobalStyles'
-import useFetch from './hooks/useFetch'
+
 function App() {
-  const result = useFetch(
+  const [search, setSearch] = useState('')
+  const [filtered, setFiltered] = useState(null)
+
+  let championListResponse = useFetch(
     'http://ddragon.leagueoflegends.com/cdn/12.18.1/data/en_US/champion.json'
   )
-  if (result) console.log(result)
+  let championArray
+  if (championListResponse) {
+    championArray = getValuesFromResponse(championListResponse)
+  }
+
+  // const championData = useFetch(
+  //   'http://ddragon.leagueoflegends.com/cdn/12.20.1/data/en_US/champion/Aatrox.json'
+  // )
+
+  const handleSearch = (e) => setSearch(e)
+
   return (
     <>
       <Routes>
-        <Route path='/champions' element={<Home list={result} />} />
+        <Route
+          path='/champions'
+          element={
+            <Home
+              championArray={championArray}
+              setSearch={setSearch}
+              handleSearch={handleSearch}
+              search={search}
+            />
+          }
+        />
         <Route path='/' element={<Navigate to='/champions' />} />
         <Route path='/champions/Aatrox' element={<Champion />} />
       </Routes>
