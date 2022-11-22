8679109f6146c7b32464b2073bf8b19fd8ede3cd
Ruben Ramirez <rramirez110911@gmail.com>
2022-11-21 21:50:59 -0800

    tests the champinData props

--- champion-search/src/pages/Champion.js
+++ champion-search/src/pages/Champion.js
@@ -1,5 +1,13 @@
-const Champion = ({ currentChampion }) => {
-  return <div>{currentChampion ? currentChampion : 'empty'}</div>
+const Champion = ({ currentChampion, championData }) => {
+  // let { currentChampion: champion } = championData
+  return (
+    <div>
+      {/* {championData ? championData.lore : 'empty'} */}
+      {championData.lore}
+      <br />
+      {championData.name}
+    </div>
+  )
 }
 
 export default Champion
