50479513f53f568b991ce030983ab30bb22ef755
Ruben Ramirez <rramirez110911@gmail.com>
2022-12-01 22:06:00 -0800

    removes image and heading tags

--- champion-search/src/components/Champion/ChampionInfo/ChampionInfo.js
+++ champion-search/src/components/Champion/ChampionInfo/ChampionInfo.js
@@ -3,58 +3,38 @@ import styled from 'styled-components'
 const ChampionInfo = ({ championData: { name, lore, title, id } }) => {
   return (
     <Wrapper>
-      <div>
-        <ImageWrapper>
-          <DefaultImage
-            src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${id}_0.jpg`}
-            alt={name}
-          />
-        </ImageWrapper>
-        <Info>
-          <Heading>
-            <Span>{title}</Span>
-            <br />
-            <Strong>{name}</Strong>
-          </Heading>
-          <Section>
-            <RoleWrapper>
-              <p>role:</p>
-              <p>Mage</p>
-            </RoleWrapper>
-            <DifficultyWrapper>
-              <p>difficulty:</p>
-              <p>Moderate</p>
-            </DifficultyWrapper>
-          </Section>
-          <InfoDivider></InfoDivider>
-          <LoreWrapper>
-            <p>{lore}</p>
-          </LoreWrapper>
-        </Info>
-      </div>
+      <Info>
+        <Section>
+          <RoleWrapper>
+            <p>role:</p>
+            <p>Mage</p>
+          </RoleWrapper>
+          <DifficultyWrapper>
+            <p>difficulty:</p>
+            <p>Moderate</p>
+          </DifficultyWrapper>
+        </Section>
+        <InfoDivider></InfoDivider>
+        <LoreWrapper>
+          <p>{lore}</p>
+        </LoreWrapper>
+      </Info>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
-  max-width: 1100px;
+  width: 100%;
+  height: 100%;
 `
-const ImageWrapper = styled.div``
-const DefaultImage = styled.img``
 const Info = styled.div`
   /* height: min-content; */
   display: flex;
   flex-flow: row nowrap;
   justify-content: space-evenly;
-  position: relative;
   /* border: 2px solid red; */
 `
-const Heading = styled.h1`
-  position: absolute;
-  top: 0;
-`
-const Span = styled.span``
-const Strong = styled.strong``
+
 const Section = styled.div`
   flex: 1 1 0;
   display: flex;
