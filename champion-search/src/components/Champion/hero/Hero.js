725fbad6015877612b4e8332fe7f7b3266f22a58
Ruben Ramirez <rramirez110911@gmail.com>
2022-12-07 21:04:05 -0800

    adds flex grow to 100% and shrink to 1

--- champion-search/src/components/Champion/hero/Hero.js
+++ champion-search/src/components/Champion/hero/Hero.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 
 const Hero = ({ championData: { name, id, title } }) => {
-  console.log(name)
   return (
     <Wrapper
       url={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${id}_0.jpg`}
@@ -16,14 +15,27 @@ const Hero = ({ championData: { name, id, title } }) => {
 }
 
 const Wrapper = styled.section`
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
-    url(${(props) => props.url}) no-repeat center fixed;
+  /* width: 100%;
+  height: 100%; */
+  flex: 100% 1 0;
+
+  background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+    url(${(props) => props.url}) no-repeat center;
   background-size: cover;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
 `
-const Heading = styled.h1``
-const Span = styled.span``
-const Strong = styled.strong``
+const Heading = styled.h1`
+  color: hsl(0, 0%, 100%);
+  margin-left: 96px;
+  margin-bottom: 96px;
+`
+const Span = styled.span`
+  font-size: 2rem;
+`
+const Strong = styled.strong`
+  font-size: 5.5rem;
+`
 
 export default Hero
