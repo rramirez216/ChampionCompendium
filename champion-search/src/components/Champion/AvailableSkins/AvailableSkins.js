c293bf8391bbcf9b30e0da2acd68bb6191a6157b
Ruben Ramirez <rramirez110911@gmail.com>
2022-12-29 23:04:57 -0800

    creates available skins component and imports swiper and swiper slide

---
+++ champion-search/src/components/Champion/AvailableSkins/AvailableSkins.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/scrollbar'
+
+const AvailableSkins = ({ championSkins, championId }) => {
+  return (
+    <Wrapper>
+      <h1>Available Skins:</h1>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        slidesPerView={1}
+        navigation
+        scrollbar={{ draggable: true }}
+      >
+        {championSkins.map((skin) => (
+          <SwiperSlide>
+            <img
+              src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championId}_${skin.num}.jpg`}
+              alt={skin.name}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  width: 100%;
+  height: 100%;
+  background-color: hsl(0 0% 100%);
+  color: hsl(0 0% 90%);
+`
+
+export default AvailableSkins
