dd62a218a9e71f5b7c498cb0fcee967daf3f92da
Ruben Ramirez <rramirez110911@gmail.com>
2023-01-02 22:23:33 -0800

    removes background image property

--- champion-search/src/pages/Champion.js
+++ champion-search/src/pages/Champion.js
@@ -3,14 +3,8 @@ import Hero from '../components/Champion/hero/Hero'
 import AvailableSkins from '../components/Champion/AvailableSkins/AvailableSkins'
 
 const Champion = ({ championData }) => {
-  let backgroundImg
-  if (championData) {
-    backgroundImg = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championData.id}_0.jpg`
-    console.log(championData.skins)
-  }
-
   return (
-    <Wrapper url={backgroundImg}>
+    <Wrapper>
       {championData ? <Hero championData={championData} /> : 'no champion Data'}
       {championData ? (
         <AvailableSkins
@@ -27,19 +21,6 @@ const Champion = ({ championData }) => {
 const Wrapper = styled.main`
   width: 100%;
   height: 100%;
-
-  background: linear-gradient(
-      to right,
-      hsl(0 0% 0% / 0.9),
-      hsl(0 0% 0% / 0.8),
-      hsl(0 0% 0% / 0.3),
-      hsl(0 0% 0% / 0.2)
-    ),
-    url(${(props) => props.url});
-
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-attachment: scroll;
   overflow-y: scroll;
 `
 
