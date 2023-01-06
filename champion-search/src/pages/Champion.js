d1c4d225ea91b2d00b759ee2caf0f2ab66f156ba
Ruben Ramirez <rramirez110911@gmail.com>
2023-01-06 10:16:28 -0800

    removes AvailableSkins directory and import

--- champion-search/src/pages/Champion.js
+++ champion-search/src/pages/Champion.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components'
 import Hero from '../components/Champion/hero/Hero'
-import AvailableSkins from '../components/Champion/AvailableSkins/AvailableSkins'
+import Skins from '../components/Champion/Skins/Skins'
 
 const Champion = ({ championData }) => {
   return (
     <Wrapper>
       {championData ? <Hero championData={championData} /> : 'no champion Data'}
       {championData ? (
-        <AvailableSkins
+        <Skins
           championSkins={championData.skins}
           championId={championData.id}
         />
@@ -22,6 +22,7 @@ const Wrapper = styled.main`
   width: 100%;
   height: 100%;
   overflow-y: scroll;
+  position: relative;
 `
 
 export default Champion
