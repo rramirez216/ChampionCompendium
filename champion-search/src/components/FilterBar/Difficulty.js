df81afd3d533c0b52125c8c1806e6fc4765d542e
Ruben Ramirez <rramirez110911@gmail.com>
2023-01-17 12:34:51 -0800

    imports dropdown and constants

--- champion-search/src/components/FilterBar/Difficulty.js
+++ champion-search/src/components/FilterBar/Difficulty.js
@@ -1,10 +1,17 @@
 import styled from 'styled-components'
 import X from './X'
+import DropDown from './DropDown'
+import constants from '../../data/constants'
 
 const Difficulty = ({ difficulty, setDifficulty }) => {
   return (
     <Wrapper>
       <Current>{difficulty}</Current>
+      <DropDown
+        list={constants.difficulty}
+        setDifficulty={setDifficulty}
+        variant={'difficulty'}
+      />
       <X
         displayBoolean={difficulty !== 'All Difficulties'}
         setDifficulty={setDifficulty}
@@ -13,9 +20,7 @@ const Difficulty = ({ difficulty, setDifficulty }) => {
   )
 }
 
-const Wrapper = styled.div`
-  display: flex;
-`
+const Wrapper = styled.div``
 const Current = styled.div``
 
 export default Difficulty
