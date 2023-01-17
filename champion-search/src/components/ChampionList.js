e35f9fa0665c4f65061f456bc2e8d998388dcc90
Ruben Ramirez <rramirez110911@gmail.com>
2023-01-16 22:53:50 -0800

    removes inner wrapper

--- champion-search/src/components/ChampionList.js
+++ champion-search/src/components/ChampionList.js
@@ -4,7 +4,6 @@ import ListItem from './ListItem'
 const ChampionList = ({ championArray, setCurrentChampion }) => {
   return (
     <Section>
-      {/* <InnerWrapper> */}
       {championArray ? (
         championArray.map((value) => (
           <ListItem
@@ -17,7 +16,6 @@ const ChampionList = ({ championArray, setCurrentChampion }) => {
       ) : (
         <div>loading...</div>
       )}
-      {/* </InnerWrapper> */}
     </Section>
   )
 }
@@ -28,10 +26,6 @@ const Section = styled.section`
   grid-template-columns: repeat(auto-fill, minmax(250px, 300px));
   justify-content: center;
   gap: 32px;
-`
-const InnerWrapper = styled.div`
-  /* max-width: 250px;
-  max-height: 350px; */
 `
 
 export default ChampionList
