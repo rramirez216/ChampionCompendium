6948aa8c9471f47092b88de15b2a34dfe69111ef
Ruben Ramirez <rramirez110911@gmail.com>
2023-01-18 22:38:46 -0800

    medea queries for the input and label

--- champion-search/src/components/FilterBar/Search.js
+++ champion-search/src/components/FilterBar/Search.js
@@ -26,12 +26,29 @@ const Search = ({ search, handleSearch, setSelected, setSearch }) => {
 
 const Label = styled.label`
   display: flex;
+  flex-flow: row;
   width: 120px;
+  @media (max-width: 34.375rem) {
+    width: 100%;
+  }
 `
 const Input = styled.input`
   border-style: none;
   font-size: 1.2rem;
-  width: 100%;
+  width: 90%;
+  margin-left: 8px;
+  outline: none;
+  &::placeholder {
+    color: hsl(0 0% 70%);
+    font-size: 0.9rem;
+  }
+  @media (max-width: 34.375rem) {
+    margin: 0;
+    &:focus {
+      border: 0.2px solid hsl(40 53% 58%);
+      width: 100%;
+    }
+  }
 `
 
 export default Search
