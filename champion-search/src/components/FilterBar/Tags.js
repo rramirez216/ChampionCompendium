2c7ffd6d2e88dcc699c279532e4f0d10856522c6
Ruben Ramirez <rramirez110911@gmail.com>
2023-01-23 21:59:45 -0800

    adds underline animation to the buttons in tags component

--- champion-search/src/components/FilterBar/Tags.js
+++ champion-search/src/components/FilterBar/Tags.js
@@ -13,22 +13,20 @@ const Tags = ({ setTag, tag }) => {
           tag={tag}
           variant={'tags'}
         >
-          <div>{tag}</div>
-          <X
-            displayBoolean={tag !== 'All Roles'}
-            location={'tags'}
-            setTag={setTag}
-          />
+          <div>{tag !== 'All' ? tag : 'All Roles'}</div>
+          <X displayBoolean={tag !== 'All'} location={'tags'} setTag={setTag} />
         </DropDown>
       </InnerWrapper>
       {constants.tags.map((value) => (
         <Button onClick={() => setTag(value.singular)} key={value.id}>
-          {value.name}
+          {value.name === 'All Roles' ? 'All' : value.name}
+          <Span></Span>
         </Button>
       ))}
     </Wrapper>
   )
 }
+// color: hsl(40 53% 58%);
 
 const Wrapper = styled.div`
   display: flex;
@@ -39,12 +37,30 @@ const Wrapper = styled.div`
     flex: 1 0 0;
   }
 `
+const Span = styled.span`
+  display: block;
+  width: 0;
+  opacity: 0;
+  margin: auto;
+  transform: translateY(8px);
+  border: 1px solid hsl(40 53% 58%);
+  transition: width 450ms ease, opacity 550ms ease-in-out;
+`
 const Button = styled.button`
   font-size: 1.2rem;
   color: hsl(0, 0%, 55%);
   background-color: hsl(0, 0%, 100%);
   border-style: none;
   cursor: pointer;
+  transition: color 350ms ease-out;
+  &:hover {
+    color: hsl(0, 0%, 10%);
+  }
+  &:hover ${Span} {
+    width: 75%;
+    opacity: 1;
+  }
+
   @media (max-width: 68.75rem) {
     display: none;
   }
