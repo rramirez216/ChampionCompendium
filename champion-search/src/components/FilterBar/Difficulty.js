e70a90249bb6a9320ffd63044d8c51743f105534
Ruben Ramirez <rramirez110911@gmail.com>
2023-01-24 20:29:47 -0800

    removes dropdown and constants import and deletes dropdown component from render function

--- champion-search/src/components/FilterBar/Difficulty.js
+++ champion-search/src/components/FilterBar/Difficulty.js
@@ -1,17 +1,10 @@
 import styled from 'styled-components'
 import X from './X'
-import DropDown from './DropDown'
-import constants from '../../data/constants'
 
 const Difficulty = ({ difficulty, setDifficulty }) => {
   return (
     <Wrapper>
       <Current>{difficulty}</Current>
-      <DropDown
-        list={constants.difficulty}
-        setDifficulty={setDifficulty}
-        variant={'difficulty'}
-      />
       <X
         displayBoolean={difficulty !== 'All Difficulties'}
         setDifficulty={setDifficulty}
